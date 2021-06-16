40175588b62afbcf1ed5451bf77c509673b47d01
Andrey <>
2021-06-16 17:15:56 +0300

    dz1

---
+++ tmsLesson2DZ/tmsLesson2DZ/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  tmsLesson2DZ
+//
+//  Created by Андрей Трофимов on 16.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //
+        
+        ///
+        
+        
+        let a ss 
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
